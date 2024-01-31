fcea1d45170e297aa668bcb536524f5b0ba5a977
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2024-01-31 13:02:56 +0100

    add settings.json

---
+++ test/dummy.test.ts
@@ -0,0 +1,7 @@
+describe('The test suite', () => {
+  it('should fail', () => {
+    expect("And it's running.. you can delete that test now.. :) ").toEqual(
+      'The test suite would run'
+    );
+  });
+});
